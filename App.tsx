c6c8dc3ddaa0dfa1929f2322883ad1cd179c9892
devsoldier <yazeedwong@gmail.com>
2025-10-20 22:16:08 +0800

    fixed vector-icons not showing

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { AuthContextProvider } from "./src/features/auth/context/auth-context";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { RootNavigator } from "./src/features/route";
-import { LoginPage } from "./src/features/auth/component/login";
-import { SignupPage } from "./src/features/auth/component/signup";
 import Toast from "react-native-toast-message";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "react-native";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [loaded] = useFonts({
+    MaterialIcons: require("@expo/vector-icons/build/vendor/react-native-vector-icons/Fonts/MaterialIcons.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <GestureHandlerRootView>
       <AuthContextProvider>
@@ -17,7 +24,6 @@ export default function App() {
           <RootNavigator />
         </SafeAreaView>
       </AuthContextProvider>
-      {/* <SignupPage></SignupPage> */}
 
       <Toast />
     </GestureHandlerRootView>
